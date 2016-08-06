11bbce8ea8af673c85c1009e009e2a56afface72
the-nick-of-time <nith2593@colorado.edu>
2016-08-06 13:32:10 -0600

    Fixes and formatting

--- pak.bat
+++ pak.bat
@@ -1,4 +1,4 @@
 set root="C:\Program Files (x86)\Steam\SteamApps\common\Starbound"
 
-%root%\win32\asset_packer.exe .\MOD %root%\mods\Fixes.modpak
+%root%\win32\asset_packer.exe .\MOD %root%\mods\Fixes.pak
 pause
