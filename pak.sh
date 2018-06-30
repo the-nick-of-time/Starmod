7213707c0a3453cb76dcd94a0671ad9fc7d668f7
Nicholas Thurmes <nith2593@colorado.edu>
2018-06-30 17:29:17 -0600

    Create FU Immortal Critters mod files

--- pak.sh
+++ pak.sh
@@ -2,6 +2,10 @@
 
 root=~/.steam/steam/steamapps/common/Starbound/linux
 
-echo Packing
+echo Packing Fixes
 $root/asset_packer ./MOD $root/../mods/Fixes.pak
+echo Finished packing!
+
+echo Packing FU Immortal Critters
+$root/asset_packer ./FU_ImmortalCritters $root/../mods/FU_ImmortalCritters.pak
 echo Finished packing!
